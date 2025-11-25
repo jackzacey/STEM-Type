f0701fbaf4205f1b6d45634d30f8684b631d3ccc
jackzacey <jackzacey@gmail.com>
2025-11-25 11:51:16 -0800

    Create terms.ts

---
+++ data/terms.ts
@@ -0,0 +1,24 @@
+export type Course =
+  | 'FreshmanBio'
+  | 'SophomoreChem'
+  | 'APBio'
+  | 'APChem'
+  | 'APPhys'
+  | 'APPhysC'
+  | 'APPhysMech'
+  | 'APES'
+  | 'APPhys2';
+
+export type Term = {
+  course: Course;
+  unit: string;
+  term: string;
+  definition: string;
+};
+
+export const allTerms: Term[] = [
+  { course: 'FreshmanBio', unit: 'Cell Biology', term: 'Mitosis', definition: 'Cell division producing identical daughter cells' },
+  { course: 'FreshmanBio', unit: 'Genetics', term: 'Allele', definition: 'Different forms of a gene' },
+  { course: 'SophomoreChem', unit: 'Chemical Bonding', term: 'Covalent Bond', definition: 'Bond formed by sharing electrons' },
+  { course: 'APBio', unit: 'Photosynthesis', term: 'Photosynthesis', definition: 'Process converting light to chemical energy' },
+];
