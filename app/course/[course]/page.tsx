8ee67d597cd3d83bb8db390006f9935f002b2c1c
jackzacey <jackzacey@gmail.com>
2025-11-26 14:31:32 -0800

    Update page.tsx

--- app/course/[course]/page.tsx
+++ app/course/[course]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { allTerms } from 'data/terms';
-import UnitSelector from 'components/UnitSelector';
+import { allTerms } from '../../data/terms';
+import UnitSelector from '../../components/UnitSelector';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
